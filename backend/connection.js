0ed253f2ad7eff62bf33ae6d7674401e7d5a64b9
RemyaUL <55387606+RemyaUL@users.noreply.github.com>
2024-07-14 10:20:57 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://userone:userone@mycluster.rxtlcxm.mongodb.net/moviedb?retryWrites=true&w=majority&appName=MyCluster').then((res)=>{
+mongoose.connect('mongodb+srv://<username>:<password@mycluster.rxtlcxm.mongodb.net/<dbname>?retryWrites=true&w=majority&appName=MyCluster').then((res)=>{
     console.log('DB is connected')
 }).catch((res)=>{
     console.log('DB not connected')
